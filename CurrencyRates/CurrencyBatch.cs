3cf84e28aa0bdcf5c15d67d10bd1958276798db7
kyrlouca@gmail.com <kyrlouca>
2022-04-25 10:29:36 +0300

    hous

--- CurrencyRates/CurrencyBatch.cs
+++ CurrencyRates/CurrencyBatch.cs
@@ -26,11 +26,43 @@ namespace CurrencyRates
 
     internal class CurrencyBatch
     {
-        public  static void OpenExcelFile(string fileName)
-        {               
+        public static void OpenExcelFile(string fileName)
+        {
         }
 
 
+        public static XSSFWorkbook? GetExcelWorkbook(string fileName)
+        {
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                return null;
+            }
+            try
+            {
+                using var stream = new FileStream(fileName, FileMode.Open);
+                stream.Position = 0;
+
+                var excelFile = new XSSFWorkbook(stream);
+                return excelFile;
+            }
+            catch (FileNotFoundException fnf)
+            {
+                Console.WriteLine($"The file:+{fileName}+ could not be found :{fnf.Message}");
+                return null;
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"The file: +{fileName}+ could not be opened: {e.Message}");
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"The file: +{fileName}+ is NOT a valid EXCEL file: {e.Message}");
+                return null;
+            }
+
+        }
 
 
         public static List<ExchangeRate> ReadExcelFile(string fileName)
@@ -40,41 +72,21 @@ namespace CurrencyRates
             var rateColIdx = -1;
             var headerRowIdx = -1;
             ISheet sheet;
-            XSSFWorkbook excelFile;
+            var rates = new List<ExchangeRate>();
 
-            var rates = new List<ExchangeRate>();            
-            if (string.IsNullOrEmpty(fileName))
+            //*************************************************************
+            //get the excelWorkbook as XSSFWorkbook
+            
+            var excelFile = GetExcelWorkbook(fileName);
+            if (excelFile == null)
             {
-                return rates;
-            }
-                        
-            try
-            {
-                using var stream = new FileStream(fileName, FileMode.Open);
-                stream.Position = 0;
-                
-                excelFile = new XSSFWorkbook(stream);                
-            }
-            catch (FileNotFoundException fnf)
-            {
-                Console.WriteLine($"The file:+{fileName}+ could not be found.{fnf.Message}");
-                return rates;
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine($"The file: +{fileName}+ could not be opened: '{e}'");
-                return rates;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"The file: +{fileName}+ is NOT a valid EXCEL file: '{e}'");
                 return rates;
             }
 
 
             //*************************************************************
             // get the header row as the first non-empty line
-            
+
             sheet = excelFile.GetSheetAt(0);
             for (var i = 0; i <= sheet.LastRowNum; i++)
             {
@@ -134,7 +146,7 @@ namespace CurrencyRates
                 }
             }
 
-            if(excelFile is not null)
+            if (excelFile is not null)
             {
                 excelFile.Close();
             }
@@ -156,7 +168,7 @@ namespace CurrencyRates
                 INSERT INTO dbo.CurrencyBatch (DateCreated, Year, Quarter, Wave ,status)  VALUES (@DateCreated, @Year, @Quarter, @Wave, @Status);
                 SELECT CAST(SCOPE_IDENTITY() as int);
             ";
-            var currencyBatchId = connectionLocal.QuerySingleOrDefault<int>(sqlInsertBatch, new { dateCreated = DateTime.Now, year, quarter, wave, status="E" });
+            var currencyBatchId = connectionLocal.QuerySingleOrDefault<int>(sqlInsertBatch, new { dateCreated = DateTime.Now, year, quarter, wave, status = "E" });
             if (currencyBatchId == 0) return 0;
 
             //*************************************************************
@@ -168,31 +180,32 @@ namespace CurrencyRates
                 try
                 {
                     connectionLocal.Execute(sqlInsertRate, new { currencyBatchId, currency = rate.CurrencyCode, exchangeRate = rate.Rate });
-                    count++; 
+                    count++;
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
-                    isValid= false;
+                    isValid = false;
                 }
             }
-            
+
             var finalStatus = count > 0 && isValid ? "S" : "E";
             var sqlUpdate = @"update CurrencyBatch set status = @status where currencyBatchId=@currencyBatchId";
-            connectionLocal.Execute(sqlUpdate, new { currencyBatchId,status=finalStatus});
+            connectionLocal.Execute(sqlUpdate, new { currencyBatchId, status = finalStatus });
             return currencyBatchId;
         }
 
         public static int CreateCurrenciesFromFile(string filename, int year, int quarter, int wave)
         {
+            var excelWorkbook = GetExcelWorkbook(filename);
             var rates = ReadExcelFile(filename);
-            var currencyBatchId=CreateCurrencyBatchData(year, quarter, wave, rates);
-            UpdateDocumentsWithCurrencyBatch(year,quarter,wave,currencyBatchId);
+            var currencyBatchId = CreateCurrencyBatchData(year, quarter, wave, rates);
+            UpdateDocumentsWithCurrencyBatch(year, quarter, wave, currencyBatchId);
             return currencyBatchId;
 
         }
 
-        public static void UpdateDocumentsWithCurrencyBatch( int year,int quarter, int wave,int currencyBatchId)
+        public static void UpdateDocumentsWithCurrencyBatch(int year, int quarter, int wave, int currencyBatchId)
         {
 
             var configObject = Configuration.GetInstance("IU260").Data;
@@ -204,9 +217,9 @@ namespace CurrencyRates
                     on doc.PensionFundId =fnd.FundId
                     where doc.ApplicableYear=@year and doc.ApplicableQuarter=@quarter and fnd.Wave=@wave;
                    ";
-                        
-            connectionLocal.Execute(sqlUpdDocument, new { year, quarter, wave,currencyBatchId });
-            
+
+            connectionLocal.Execute(sqlUpdDocument, new { year, quarter, wave, currencyBatchId });
+
         }
     }
 }
