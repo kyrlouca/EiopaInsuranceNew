6a42120eca1fdad6bfc113e0c005684211314c5a
kyrlouca@gmail.com <kyrlouca>
2023-02-07 18:47:49 +0200

    aa

--- XbrlReader/Program.cs
+++ XbrlReader/Program.cs
@@ -20,8 +20,8 @@ namespace XbrlReader
             //C:\Users\kyrlo\soft\dotnet\insurance-project\TestingXbrl260\final\ALTIUS INSURANCE LTD Q1 2022.xbrl
             //C:\Users\kyrlo\soft\dotnet\insurance-project\TestingXbrl270\Universal.xbrl
             (var fundIdT, var filePath) = (44, @"C:\Users\kyrlo\soft\dotnet\insurance-project\TestingXbrl270\Universal.xbrl");
-            //XbrlFileReader.ProcessXbrlFile("IU260", 1, 2, fundIdT, "ars", 2021, 0,filePath);
-            XbrlGenerator.GenerateXbrlFile("IU270", 1, 2, fundIdT, "qrs", 2022, 4, filePath);
+            XbrlFileReader.ProcessXbrlFileNew("IU270", 1, 2, fundIdT, "ars", 2021, 0,filePath);
+            //XbrlGenerator.GenerateXbrlFile("IU270", 1, 2, fundIdT, "qrs", 2022, 4, filePath);
 
             Console.WriteLine("Finish");
             return 1;
